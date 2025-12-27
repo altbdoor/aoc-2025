6f5c8c03a9a39cd7b0258b5d760ea94e1229a982
altbdoor <lancersupraskyline@gmail.com>
2025-12-27 14:21:15 +0800

    finish day 2 part 2

--- 02/main.go
+++ 02/main.go
@@ -35,14 +35,14 @@ func main() {
 	}
 }
 
-type Handle1Coord struct {
+type HandleCoord struct {
 	start int
 	stop  int
 }
 
 func handle1(file *os.File) {
 	scanner := bufio.NewScanner(file)
-	var coordList []Handle1Coord
+	var coordList []HandleCoord
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -58,7 +58,7 @@ func handle1(file *os.File) {
 			startVal, _ := strconv.Atoi(cleanedArr[0])
 			stopVal, _ := strconv.Atoi(cleanedArr[1])
 
-			coord := Handle1Coord{
+			coord := HandleCoord{
 				start: startVal,
 				stop:  stopVal,
 			}
@@ -112,8 +112,75 @@ func handle1(file *os.File) {
 
 func handle2(file *os.File) {
 	scanner := bufio.NewScanner(file)
+	var coordList []HandleCoord
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(strconv.Atoi(line))
+		currentText := strings.Split(line, ",")
+
+		for _, text := range currentText {
+			cleanedText := strings.TrimSpace(text)
+			if cleanedText == "" {
+				continue
+			}
+
+			cleanedArr := strings.SplitN(cleanedText, "-", 2)
+			startVal, _ := strconv.Atoi(cleanedArr[0])
+			stopVal, _ := strconv.Atoi(cleanedArr[1])
+
+			coord := HandleCoord{
+				start: startVal,
+				stop:  stopVal,
+			}
+			coordList = append(coordList, coord)
+		}
 	}
+
+	sumOfBrokenIds := 0
+
+	for _, coord := range coordList {
+		fmt.Printf("group %d to %d\n  >> ", coord.start, coord.stop)
+		checkInt := coord.start - 1
+
+		for checkInt != coord.stop {
+			checkInt += 1
+
+			// single digits will not repeat
+			if checkInt < 10 {
+				continue
+			}
+
+			checkStr := strconv.Itoa(checkInt)
+			checkStrLen := len(checkStr)
+
+			// check for all same repeats
+			if checkStr == strings.Repeat(checkStr[:1], checkStrLen) {
+				fmt.Printf("%d, ", checkInt)
+				sumOfBrokenIds += checkInt
+				continue
+			}
+
+			// assume lower boundary of the half length
+			halfCheckStrLen := checkStrLen / 2
+
+			// loop from 2 chars until `len(n)/2` chars
+			for i := 2; i <= halfCheckStrLen; i++ {
+				checkStrSub := checkStr[:i]
+				checkStrSubLen := len(checkStrSub)
+				repeatCounter := checkStrLen / checkStrSubLen
+
+				if checkStr == strings.Repeat(checkStrSub, repeatCounter) {
+					fmt.Printf("%d, ", checkInt)
+					sumOfBrokenIds += checkInt
+					break
+				}
+			}
+		}
+
+		fmt.Println()
+		fmt.Println()
+	}
+
+	fmt.Printf("sum: %d", sumOfBrokenIds)
+	fmt.Println()
 }
